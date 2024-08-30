ddec4b375d614c551040c647fe07314c211ecf79
jecortes2304 <jecortes2304@gmail.com>
2024-08-30 15:04:42 +0200

    Initial commit

---
+++ src/main/java/com/cortestudios/pricingmanager/PricingManagerApplication.java
@@ -0,0 +1,13 @@
+package com.cortestudios.pricingmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PricingManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PricingManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cortestudios/pricingmanager/PricingManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cortestudios.pricingmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PricingManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
